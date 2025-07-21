23c433c38123b6a2d7c6aec03857338725ed0c2d
添川知紘 <soikawachihiro@soekawachikonoMacBook-Air.local>
2025-07-21 17:52:07 +0900

    axios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  buildModules: [
+  modules: [
     '@nuxtjs/vuetify',
-    '@nuxtjs/dotenv', 
+    '@nuxtjs/dotenv'
   ],
-  server: {
-    port: 8080 
-  },
 })
